c7accdd1891ae1aec429ab4743ed8412cb862bf0
taahachaudhry <t44h413@gmail.com>
2014-08-07 16:50:08 -0500

    Created outline for chirps/chirps array and object

--- Chirper/_script.js
+++ Chirper/_script.js
@@ -1,1 +1,23 @@
-﻿
+﻿var Chirper = {};
+//Firebase base
+Chirper.base = "htcchirper";
+
+//Array of chirps and Chirp object
+Chirper.chirps = [];
+Chirper.Chirp = function (name, message) {
+    this.name = name;
+    this.message = message;
+    this.timestamp = timestamp;
+}
+//CRUD functions with AJAX call to Firebase
+Chirper.create = function () { };
+Chirper.read = function () { };
+Chirper.edit = function () { };
+Chirper.save = function () { };
+Chirper.delete = function () { };
+
+//Table outputting chirps
+Chirper.output = function () { };
+
+//URL Helper for Firebase
+//AJAX Call Function
